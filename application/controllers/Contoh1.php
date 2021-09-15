7dbaa60c772be1a0adc5a39dcc888cb2223ed33e
nurulhikmah28 <nurulhikmah280512@gmail.com>
2021-09-15 23:51:09 +0700

    contoh1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Nurul Hikmah,
+            <br> Saya tinggal di Kelapa Gading 
+            <br> Olah Raga yang saya sukai adalah Bulutangkis";
+    }
+}
+?>
