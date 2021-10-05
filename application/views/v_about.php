248a7dc62e99438a28a5a8508557af596af6d6de
nurulhikmah28 <nurulhikmah280512@gmail.com>
2021-10-05 22:45:38 +0700

    Latihan Membuat Template Web Sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Nurul</li>
+        <li>Nama Belakang : Hikmah</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Perjuangan Raya No.29 Jakarta Utara</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Cilacap</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>football</li>
+    </ul>
+
+</section>
