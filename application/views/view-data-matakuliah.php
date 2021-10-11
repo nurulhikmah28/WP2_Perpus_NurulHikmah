8dfb5e2c431acb07ef9819c9498177340885fb10
nurulhikmah28 <nurulhikmah280512@gmail.com>
2021-10-11 08:52:35 +0700

    Latihan Membuat Form Validasi pada Codeigniter

---
+++ application/views/view-data-matakuliah.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+    <title>Tampil Data Matakuliah</title>
+</head>
+
+<body>
+    <center>
+        <table>
+            <tr>
+                <th colspan="3">
+                    Tampil Data Mata Kuliah
+                </th>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <hr> 
+                </td>
+            </tr>
+            <tr>
+                <td>Kode MTK</td>
+                <th>:</th>
+                <td>
+                    <?= $kode; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Nama MTK</td>
+                <th>:</th>
+                <td>
+                    <?= $nama; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>SKS</td>
+                <th>:</th>
+                <td>
+                    <?= $sks; ?>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3" align="center">
+                    <a href="<?= base_url('Matakuliah');?>">kembali</a>
+                </td>
+            </tr>
+        </table>
+    </center>
+</body>
+
+</html>
